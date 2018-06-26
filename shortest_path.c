eca1c3ec322e813517807086912a97a1b1e0cf13
Denys GONOR <dgonor@e1r2p14.unit.ua>
2018-06-26 15:11:19 +0300

    Tue Jun 26 15:11:19 EEST 2018

--- shortest_path.c
+++ shortest_path.c
@@ -12,17 +12,83 @@
 
 #include "ft_lem.h"
 
+int	lstlast_link_queue(t_link **lst)
+{
+    t_link *tmp;
 
+    tmp = NULL;
+    if (lst)
+    {
+        tmp = *lst;
+        while (tmp->next != NULL)
+        {
+            tmp = tmp->next;
+        }
+    }
+    return (tmp->num);
+}
+
+void	lstback_link_queue(t_link **lst, t_link *add)
+{
+    t_link *tmp;
+
+    tmp = NULL;
+    if (lst && add)
+    {
+        tmp = *lst;
+        while (tmp->next != NULL)
+        {
+            tmp = tmp->next;
+        }
+        tmp->next = add;
+    }
+}
+
+t_link      *create_queue(t_game *data)
+{
+    t_link  *new;
+
+    if (!(new = (t_link*)malloc(sizeof(t_link) * 1)))
+    {
+        data->error = 3;
+        return (NULL);
+    }
+    new->next = NULL;
+    return (new);
+}
 
 void        find_path(t_game * data)
 {
-    t_list     *queue;
-    int         *i;
+    t_link     *queue;
+    t_link     *next;
+    t_link     *tmp;
+    t_room     *tmp_room;
+    int         i;
+    int         n;
 
-    i = (int*)malloc(sizeof(int) * 1);
-    *i = 3;
-    queue = ft_lstnew(i, 1);
+    i = 0;
     data->visited = ft_strnew(data->nroom);
-    ft_lstadd(queue, ft_lstnew(i, 1));
+    ft_memset(data->visited, '0', data->nroom);
+    queue = NULL;
+    tmp = data->room[data->start].link;
+    while (1)
+    {
+        while (tmp)
+        {
+            next = create_queue(data);
+            next->num = tmp->num;
+            if (!queue)
+                queue = next;
+            else if (data->visited[queue->num] = '0')
+            {
+                lstback_link_queue(&queue, next);
+                data->visited[queue->num] = '1';
+            }
+            tmp = tmp->next;
+        }
+        tmp_room = &data->room[queue->num];
+        queue = queue->next;
+        tmp = data->room[queue->num].link;
+    }
 
 }
